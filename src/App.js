9af20cc0af998ed68a2b73c512c6b4a8b3a2fbc6
Victor Machado Emerick <emerick_machado@hotmail.com>
2019-05-31 10:57:01 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      pilha: Pilha
+    }
+  }
+
+  render(){
+
+    /*return (
+      <div className="App">
+      </div>
+    );*/
+return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
